e8d41bb353abf224a4f0f4d3a33ff5461b4e68d4
uncleA <lotar73@gmail.com>
2023-11-24 23:12:58 +0300

    Initial commit

---
+++ resources/js/Pages/Post/Create.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="w-96 mx-auto pt-6">
+    <h1 class="text-lg mb-8">Новое сообщение</h1>
+    <form>
+        <div>
+            <input class="mb-2 w-full rounded-full border border-gray-300" type="text" placeholder="Заголовок">
+        </div>
+        <div>
+            <textarea class="mb-2 w-full rounded-full border border-gray-300" name="" placeholder="Содержание"></textarea>
+        </div>
+        <div class="flex justify-end">
+            <button class="block hover:bg-white hover:text-sky-500 border border-sky-500 bg-sky-500 rounded-full text-center text-white p-2 w-32" type="submit">Сохранить</button>
+        </div>
+    </form>
+</div>
+</template>
+
+<script>
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    name: "Create",
+    components:{
+        Link
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="w-96 mx-auto pt-6">
+    <h1 class="text-lg mb-8">Posts</h1>
+    <div>
+        <Link :href="route('post.create')" class="block hover:bg-white hover:text-sky-500 border border-sky-500 bg-sky-500 rounded-full text-center text-white p-2 w-32">Add Post</Link>
+    </div>
+</div>
+</template>
+
+<script>
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    name: "Index",
+    components:{
+        Link
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
