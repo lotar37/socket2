9b05be75062422eaea20cd39026ff4def14a2413
Home comp <lotar37@gmail.com>
2023-11-28 15:11:14 +0300

    Вывод всех постов в индексе

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,16 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Resources\Post\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return inertia('Post/Index');
+        $posts = Post::all();
+        $posts = PostResource::collection($posts)->resolve();
+        return inertia('Post/Index', compact('posts'));
     }
     public function create()
     {
         return inertia('Post/Create');
+    }
+   public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        Post::create($data);
+        return redirect()->route('post.index');
+    }
+
+    public function getPosts()
+    {
+        return Post::all();
     }
 }
--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="w-96 mx-auto pt-6">
     <h1 class="text-lg mb-8">Новое сообщение</h1>
-    <form>
+    <form @submit.prevent="store">
         <div>
-            <input class="mb-2 w-full rounded-full border border-gray-300" type="text" placeholder="Заголовок">
+            <input class="mb-2 w-full rounded-full border border-gray-300 " v-model="title" type="text" placeholder="Заголовок">
         </div>
         <div>
-            <textarea class="mb-2 w-full rounded-full border border-gray-300" name="" placeholder="Содержание"></textarea>
+            <textarea class="mb-2 w-full rounded-full border border-gray-300" v-model="content" placeholder="Содержание"></textarea>
         </div>
         <div class="flex justify-end">
             <button class="block hover:bg-white hover:text-sky-500 border border-sky-500 bg-sky-500 rounded-full text-center text-white p-2 w-32" type="submit">Сохранить</button>
@@ -22,7 +22,20 @@ export default {
     name: "Create",
     components:{
         Link
+    },
+    data() {
+        return {
+            title: '',
+            content:'',
+
+        }
+    },
+    methods:{
+        store() {
+            this.$inertia.post('/posts', {title:this.title, content : this.content})
+        },
     }
+
 }
 </script>
 
